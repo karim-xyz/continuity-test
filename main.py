d5cb78df73d5543f70e188b57a3c351a70f98029
Abdelkarim <abdelkarim2dev@gmail.com>
2026-03-06 05:46:41 +0100

    clear code

--- main.py
+++ main.py
@@ -16,17 +16,11 @@ domain = continuous_domain(f, x, Reals)
 f_c = f.subs(x, c)
 lim_f_x = limit(f, x, c)
 
-pretty_print(f)
-print()
-print("f(c) = ", f_c)
-print()
-print("lim f(x) = ", limit(f, x, c, "+"), f"\nx->{c}+")
-print()
-print("lim f(x) = ", limit(f, x, c, "-"), f"\nx->{c}-")
-print()
-
+#f(c) exists
 cond1 = c in domain
+#f has a limit as x->c
 cond2 = limit(f, x, c, "+") == limit(f, x, c, "-")
+#the limit equals the function value
 cond3 = f_c == lim_f_x
 
 if cond1 and cond2 and cond3:
